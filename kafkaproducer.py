04bf38ab4eb91a019c7d41c296ce10c32dbbc390
Simon Aubury <response_github@vsimon.com>
2023-02-21 18:07:26 +1100

    cleanup

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -22,28 +22,12 @@ def kafka_producer():
     producer = AvroProducer(producer_config, default_value_schema=value_schema)
     return 'mastodon-topic', producer
 
-    # try:
-        
-
-    #     value_dict = {  'language': 'en', 'favourites': 0, 'username': 'bob', 'bot': False, 'tags': 0, 'characters': 50, 'words': 12}
-
-    #     producer.produce(topic = 'mastodon-topic', value = value_dict)
-    #     producer.flush()
-
-    # except KafkaException as e:
-    #     print('Kafka failure ' + e)
-
-
-
 def main():
+    # example test producer
     topic_name, producer = kafka_producer()
 
     value_dict = {  'language': 'en', 'favourites': 0, 'username': 'bob', 'bot': False, 'tags': 0, 'characters': 50, 'words': 12}
     producer.produce(topic = topic_name, value = value_dict)
-
-    value_dict = {  'language': 'fr', 'favourites': 0, 'username': 'jane', 'bot': False, 'tags': 0, 'characters': 500, 'words': 120}
-    producer.produce(topic = topic_name, value = value_dict)
-
     producer.flush()
 
 if __name__ == '__main__':
